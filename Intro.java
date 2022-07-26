b08548b49d688ee4e037a43dbcd2fe995946182e
Megha631 <109948266+Megha631@users.noreply.github.com>
2022-07-26 13:35:29 +0530

    Intro.java

---
+++ Intro.java
@@ -0,0 +1,76 @@
+/*Q3 a-->Define.java file and .class in java.
+.java file compile the code and .class file compile the code and show the error
+
+
+Q3 b--> java is complete OOPS programming or not?
+java is not complete OOPS programming language because in which primitive data types like float,int etc.
+java programmer used primitive data type as an object. the primitive data type like float,int are fast as compared to object.
+so that java is not complete OOPS programming language.
+
+Q3 c give example of NR-WA
+//NR-WA means no return-with parameter 
+fpr ex--> void A(int a){
+           int a=6;
+          System.out.println("the value of a is" +a);   
+		  }  */
+/*Q5--> Wrapper classes.
+ wrapper classes is in which we use capital letter for data type.
+ for example-->Integer a=56;*/	
+ 
+ /*Q2 naming convention
+ Class name=My school{
+           }
+ Function name=My bag(){
+	 System.out.println("this is my bag");
+     }
+ Static variable=Static static int a=7;  */
+ 
+//Q1		  
+/*class Intro{
+    int name;
+    int age;
+     public static void main(String[]args){
+     System.out.println( "megha");
+	 System.out.println("20");
+	/*hobby h=new hobby();
+	 h.hobby();
+	}
+   void hobby(){
+	System.out.println("reading book"); 
+	}
+} */
+
+//Q4
+/*class calci{
+	//static menber fun
+	
+	 void static addy(){
+		System.out.println("i am a student");
+	 }
+	 void static subby(){
+      System.out.println("java");
+	 }
+	 void diviji(){
+		 System.out.println("core");
+	 }
+	 void mull(){
+		 System.out.println("subject");
+	 }
+	}
+	class calciworking{ 
+	public static void main(String[] args){
+		
+		
+		calci obj=new calci();
+		calci.addy();
+		calci.subby();
+		obj.diviji();
+		obj.mull();
+	}
+	}   */
+	
+	 
+	 
+	 
+	 
+	
