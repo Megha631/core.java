93dbfd078e36851973559a28aa2385b89d02de6a
Megha631 <109948266+Megha631@users.noreply.github.com>
2022-08-02 13:44:20 +0530

    Create TestP1.java
    TestP1.java

---
+++ TestP1.java
@@ -0,0 +1,122 @@
+//Q8-->print pattern
+import java.util.Scanner;
+class TestP1{
+      public static void main(String[] args){
+     /* int n=3;
+      int m=5;
+      for(int i=1;i<=n;i++){
+         for(int j=1;j<=m;j++){
+          if(i%2==0 && j%2==0 || i%2==1 && j%2==1){
+             System.out.print("1");
+             }else{
+               System.out.print("0");
+              }
+         }
+		 System.out.println();
+     }*/
+/*Q6 print number
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter the number");
+   int a=sc.nextInt();
+   for(int i=1;i<=a;i++){
+	   System.out.println(i);
+   }
+   System.out.println(a);*/
+/*Q4 switch
+int rollno=546;
+switch(rollno){
+   case 579: System.out.println("this is sonu rollno");
+    break;
+   case 546: 	System.out.println("this is monu rollno");
+    break;
+   case 553: System.out.println("this is manu rollno");
+    break;
+   case 544: 	System.out.println("this is meenu rollno");
+    break;
+   case 516: System.out.println("this is meena rollno");
+    break;
+   default:System.out.println("not allowed");
+    break;
+    }*/
+/*Q5 pass by value and reference
+ pass by value in which this process in which the function parameter is copied another variable.
+ and the instead of object copied is passed is called pass by value.
+ example=class Pv{
+	         public static void main(String [] args){
+				 int a=9;
+				 static void change(int a){
+					 System.out.println(a);
+					 a=60;
+					 System.out.println(a);
+					 chnage(a);
+					 
+ pass by reference in which the actual reference is passed.
+ example= class Pv{
+	         public static void main(String [] args){
+				M m=new M();
+				m.change();
+				System.out.println(m.age());
+                
+             class M{
+                 int a=9;
+				 int age=20;	
+			    }
+                static void change(int a){
+					 System.out.println(a);
+					 a=60;
+					 System.out.println(a);
+				    }
+                static void changeNp(int k)	{
+                    System.out.println(k.age());
+                    age=80;
+                    System.out.println(k.age());
+				  }	   				*/
+ /*Q1 check no.even or odd
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the number");
+ int k=sc.nextInt();
+ if(k%2==0){
+	 System.out.println("number is even");
+ }else{
+	 System.out.println("number is odd");
+ }*/
+/*Q2 greater or not
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the number");
+ int l=sc.nextInt(); 
+ if(l>55){
+	 System.out.println("Number is greater ");
+ }else if(l==55){
+	 System.out.println("number is equal");
+ }else {
+	 System.out.println("number is smalller");
+ }*/
+ /*Q3 greater or equal
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the first number");
+  int l=sc.nextInt(); 
+  System.out.println("enter the second number");
+  int m=sc.nextInt();
+  if(l==m){
+	  System.out.println("equal");
+  }else if (l>m && m>l){
+  System.out.println("greater");
+  }else{
+	  System.out.println("wrong");
+  }*/
+  /*Q7 array
+  int e[]=new int[10];
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the element");
+  int a=sc.nextInt();
+  for(int i=1;i<10;i++){
+	  
+  System.out.println( a=sc.nextInt());
+  
+  
+  
+      }
+	 
+
+  }
+}*/
